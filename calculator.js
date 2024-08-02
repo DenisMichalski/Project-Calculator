2d3485e2c9ea1ebdb461e2b60c8716e63f9fa85b
Denis11m <denissmi87@gmail.com>
2024-08-02 16:36:16 +0200

    adding functions for caluculation operations

---
+++ calculator.js
@@ -0,0 +1,20 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(add(2, 3));
+console.log(subtract(10, 4));
+console.log(multiply(5, 4));
+console.log(divide(50, 10));
