6baeaef7dca2c34d64cac5fc7aba7a845ff9d471
Denis11m <denissmi87@gmail.com>
2024-08-04 04:08:55 +0200

    add HTML and CSS but not ready

--- calculator.js
+++ calculator.js
@@ -14,7 +14,25 @@ function divide(a, b) {
     return a / b;
 }
 
-console.log(add(2, 3));
-console.log(subtract(10, 4));
-console.log(multiply(5, 4));
-console.log(divide(50, 10));
+// console.log(add(2, 3));
+// console.log(subtract(10, 4));
+// console.log(multiply(5, 4));
+// console.log(divide(50, 10));
+
+
+let firstNumber;
+let secondNumber;
+let operator = [add, subtract, multiply, divide];
+
+
+function operate() {
+    if (operator === add) {
+        firstNumber += secondNumber;
+    } else if (operator === subtract) {
+        return firstNumber -= secondNumber;
+    } else if (operator === multiply) {
+        return firstNumber *= secondNumber;
+    } else {
+        return firstNumber /= secondNumber;
+    }
+}
